067946217f471f9fc8a8748b642ab26c06388881
github username <github email address>
2019-07-16 17:28:09 -0700

    create a simple litter form

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  const activeStyle = { color: "#F15B2A" };
+  const activeStyle = { color: "#7b8db0" };
   return (
     <nav>
       <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>{" | "}
-      <NavLink to="/kitten/:id" activeStyle={activeStyle} exact>Kitten</NavLink>
+      <NavLink to="/kitten/:id" activeStyle={activeStyle} exact>Kitten</NavLink>{" | "}
+      <NavLink to="/litter/new" activeStyle={activeStyle} exact>Add Litter</NavLink>
     </nav>
   )
 
--- src/components/litters/LitterForm.js
+++ src/components/litters/LitterForm.js
@@ -1,12 +1,45 @@
 import React from "react";
 
-const LitterForm = () => (
-  <div>
-    <h2>LitterForm</h2>
-    <p>
-      LitterForm Page.
-    </p>
-  </div>
-);
+class LitterForm extends React.Component {
+  state = {
+    litter: {
+      name: ""
+    }
+  }
+  
+
+  handleChange = (event) => {
+    const litter = { ...this.state.litter, name: event.target.value }
+    this.setState({ litter })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    alert(this.state.litter.name)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+              <h5 className="gray-text text-darken-d">Add Litter</h5>
+          <div className="">
+            <label htmlFor="name">Litter Name / Litter ID</label>
+            <input 
+            type="text" 
+            id="name" 
+            onChange={this.handleChange}
+            value={this.state.litter.name}
+             />
+          </div>
+          
+          <div>
+            <input type="submit" value="Save" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default LitterForm;
